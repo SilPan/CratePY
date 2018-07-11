b5be80af27fc2eb47fc0d9fb81b08b7c50c905c1
leo <e01624280@student.tuwien.ac.at>
2018-07-11 13:54:29 +0200

    implement simple cratDB operations

---
+++ main.py
@@ -0,0 +1,32 @@
+from crate import client
+
+connection = client.connect("http://localhost:4200", username="crate")
+
+cursor = connection.cursor()
+
+try:
+    cursor.execute("CREATE TABLE IF NOT EXISTS strings (str STRING, num INTEGER)")
+except:
+    print("could not create table")
+
+cursor.execute("INSERT INTO strings (str, num) VALUES (?, ?)", ("Hello world!", 42))
+
+cursor.execute("INSERT INTO strings (str, num) VALUES (?, ?)", ("hi there", 99))
+
+cursor.execute("REFRESH TABLE strings")
+
+cursor.execute("SELECT str FROM strings")
+
+try:
+    print(cursor.fetchall())
+except:
+    print("no results there :(")
+
+cursor.execute("SELECT str FROM strings WHERE num > 50")
+
+try:
+    print(cursor.fetchall())
+except:
+    print("no results there :(")
+
+cursor.execute("DROP TABLE strings")
