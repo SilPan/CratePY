9dfb9c7ae3673bed80094c7b90522dd457dbc698
leo <e01624280@student.tuwien.ac.at>
2018-07-11 14:42:57 +0200

    add update statement

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from crate import client
+from crate.client.cursor import Cursor
 
 connection = client.connect("http://localhost:4200", username="crate")
 
-cursor = connection.cursor()
+cursor = connection.cursor()  # type: Cursor
 
 try:
     cursor.execute("CREATE TABLE IF NOT EXISTS strings (str STRING, num INTEGER)")
@@ -23,6 +24,17 @@ except:
     print("no results there :(")
 
 cursor.execute("SELECT str FROM strings WHERE num > 50")
+
+try:
+    print(cursor.fetchall())
+except:
+    print("no results there :(")
+
+cursor.execute("UPDATE strings SET str = ?, num = ? WHERE num > 50", ("some different string", 102))
+
+cursor.execute("REFRESH TABLE strings")
+
+cursor.execute("SELECT str FROM strings WHERE str LIKE ?", ["some different string"])
 
 try:
     print(cursor.fetchall())
